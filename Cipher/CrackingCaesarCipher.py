78a57c8a4b4c96013f7f21dd673e57d1fe55c390
Muhammad Ali Khalil <muhammadalikhalil@gmail.com>
2025-03-04 15:01:27 +0500

    Cracking Caesar Cipher

---
+++ Cipher/CrackingCaesarCipher.py
@@ -0,0 +1,19 @@
+def cracking_cipher(ciphertext):
+    alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ciphertext = ciphertext.upper()
+    
+    for key in range(1, 26):
+        decrypted = ''
+        for alphabet in ciphertext:
+            if alphabet in alphabets:
+                index = alphabets.index(alphabet)
+                new_index = (index - key) % 26
+                decrypted += alphabets[new_index]
+            else:
+                decrypted += alphabet
+        print(f'Key {key}: {decrypted}')
+                    
+    
+
+cipher_text = input("Enter Cipher Text: ")
+cracking_cipher(cipher_text)
--- Cipher/cracking.py
+++
@@ -1,1 +0,0 @@
-print("hello cracking here")
